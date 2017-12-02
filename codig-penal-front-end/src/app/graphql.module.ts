6f950259ed4ede32d3bb14bd8a6e0c6434893b04
Gian26 <giancuello26@gmail.com>
2017-12-02 02:01:32 -0600

    Changes to html layout and styles

--- codig-penal-front-end/src/app/graphql.module.ts
+++ codig-penal-front-end/src/app/graphql.module.ts
@@ -6,8 +6,10 @@ import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // GraphiQL: https://launchpad.graphql.com/1jzxrj179
-const uri = 'http://127.0.0.1:8000/graphql';
+// const uri = 'http://ec2-54-153-78-22.us-west-1.compute.amazonaws.com:8888/graphql';
 // const uri ='https://1jzxrj179.lp.gql.zone/graphql';
+const uri ='http://127.0.0.1:8888/graphql';
+
 @NgModule({
   exports: [
     HttpClientModule,
